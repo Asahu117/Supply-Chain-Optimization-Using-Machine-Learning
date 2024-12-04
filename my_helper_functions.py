fd19afcbb11b2cb493097e7541529a0aa049e17a
Asahu117 <112420597+Asahu117@users.noreply.github.com>
2024-12-04 20:35:18 +0530

    Update my_helper_functions.py

--- my_helper_functions.py
+++ my_helper_functions.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Aug  8 20:18:15 2017
 
-@author: Tichakunda Mangono
-"""
 
 # In the future, consider the following..
 # Class for datetimes already exists
